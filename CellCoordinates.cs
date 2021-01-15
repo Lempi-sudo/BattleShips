478527c45e1a8a06c94d5b1fe9c7b145bf1ea3f0
Andrei <andreii.lapschow1999@mail.ru>
2021-01-15 21:27:45 +0400

    вынес класс (координаты клетки) CellCoordinates в отдельный файл

---
+++ CellCoordinates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips
+{
+    class СellCoordinates
+    {
+        private int horizontal;
+        private int vertical;
+        public int Horizontal
+        {
+            get
+            {
+                return horizontal;
+            }
+
+        }
+        public int Vertical
+        {
+            get
+            {
+                return vertical;
+            }
+
+        }
+        public СellCoordinates(int h, int v)
+        {
+            horizontal = h;
+            vertical = v;
+        }
+
+    };
+}
