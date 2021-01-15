edb33e4c74c0ae7e86c6c68f4b9d96d26657ecb7
Andrei <andreii.lapschow1999@mail.ru>
2021-01-15 19:18:39 +0400

    Добавил основной класс игра -Game

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips
+{
+    class Game
+    {
+        private AbstractGamer firstGamer;
+        private AbstractGamer secondGamer;
+
+        private IMap mapFirstGamer;
+        private IMap mapSecondGamer;
+
+        public Game(AbstractGamer firstGamer, AbstractGamer secondGamer)
+        {
+            this.firstGamer = firstGamer;
+            this.secondGamer = secondGamer;
+
+            this.mapFirstGamer = firstGamer.DrawMap();
+            this.mapSecondGamer = secondGamer.DrawMap();
+            
+           
+
+        }
+
+        
+
+    }
+}
