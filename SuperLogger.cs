f5d9f31582117cd91e2c72e9338b800d60ed34f4
Andrei <andreii.lapschow1999@mail.ru>
2021-01-23 13:11:34 +0400

    изменил протоколирование так как пошаговая запись в файл слишком долгая . Новый класс SuperLogger создает одну строку и записывает в файл

---
+++ SuperLogger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BattleShips
+{
+    class SuperLogger
+    {
+        private string fileName;
+        private string text;
+        
+        public SuperLogger(string filename)
+        {
+            this.fileName = filename;
+        }
+        public void WriteShot(СellCoordinates cell, ResultShot resultshot)
+        {
+            text += "horizontal:";
+            text += cell.Horizontal.ToString();
+            text += "  vertical:";
+            text += cell.Vertical.ToString();
+            switch (resultshot)
+            {
+                case ResultShot.Miss:
+                    text+=" Miss";
+                    break;
+                case ResultShot.Damage:
+                    text += " Damage";
+                    break;
+                case ResultShot.Kill:
+                    text += " Kill";
+                    break;
+                default:
+                    break;
+            }
+            text += "\r\n";
+        }
+        public void WriteGamer(string strnamegamer)
+        {
+            text += strnamegamer += " Move:";
+            text += "\r\n";
+        }
+            
+
+
+        public void WriteWinner(string strnamegamer)
+        {
+            text += strnamegamer += " is winner";
+            text += "\r\n";
+            text += "\r\n";
+            text += "\r\n";
+        }
+
+        public void WriteInFile()
+        {
+            using (var sw = new StreamWriter(this.fileName , true, Encoding.UTF8))
+            {
+                sw.Write(this.text);
+            
+            }
+        }
+           
+         
+
+    }
+}
+
