fc45a4ec9bbde19036e42bea1cf68b2c66ffb375
Andrei <andreii.lapschow1999@mail.ru>
2021-01-30 14:59:11 +0400

    Logger.cs

--- BattleShips/Logger.cs
+++ BattleShips/Logger.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
+using System.IO;
 
 namespace BattleShips
 {
     /// <summary>
-    /// Данный класс не используется так как записывать в файл каждый ход по отдельности оказалось 
-    /// слишком долго . используется класс SuperLogger
+    /// Класс для логирования игры в текстовый документ 
     /// </summary>
     class Logger
     {
         /// <summary>
-        /// имя файла для записи игры 
+        /// имя файла в который будет происходить запись игры 
         /// </summary>
         private string fileName;
+        /// <summary>
+        /// Во время партии в данную переменную происходит запись ходов и результатов поавданий 
+        /// а затем по окончанию партии из этой переменной данные записываются в файл
+        /// </summary>
+        private string text;
         
         public Logger(string filename)
         {
             this.fileName = filename;
-             
         }
 
         /// <summary>
@@ -27,55 +30,59 @@ namespace BattleShips
         /// </summary>
         /// <param name="cell">координаты клетки в которую стреляли</param>
         /// <param name="resultshot">результат выстрела </param>
-        public void  WriteShot(СellCoordinates cell, ResultShot resultshot)
+        public void WriteShot(СellCoordinates cell, ResultShot resultshot)
         {
-            using(var sw = new StreamWriter(fileName, true, Encoding.UTF8))
+            text += "horizontal:";
+            text += cell.Horizontal.ToString();
+            text += "  vertical:";
+            text += cell.Vertical.ToString();
+            switch (resultshot)
             {
-                sw.Write("horizontal:{0} , vertical:{1} : ", cell.Horizontal, cell.Vertical);
-                switch (resultshot)
-                {
-                    case ResultShot.Miss:
-                        sw.Write("Miss");
-                        break;
-                    case ResultShot.Damage:
-                        sw.Write("Damage");
-                        break;
-                    case ResultShot.Kill:
-                        sw.Write("Kill");
-                        break;
-                    default:
-                        break;
-                }
-                sw.WriteLine();
+                case ResultShot.Miss:
+                    text+=" Miss";
+                    break;
+                case ResultShot.Damage:
+                    text += " Damage";
+                    break;
+                case ResultShot.Kill:
+                    text += " Kill";
+                    break;
+                default:
+                    break;
             }
-
-          
+            text += "\r\n";
         }
 
+        
         public void WriteGamer(string strnamegamer)
         {
-            using (var sw = new StreamWriter(fileName, true, Encoding.UTF8))
-            {
-                sw.Write("{0} Move:",strnamegamer);
-                sw.WriteLine();
-            
-            }   
-
+            text += strnamegamer += " Move:";
+            text += "\r\n";
         }
+            
+
 
         public void WriteWinner(string strnamegamer)
         {
-            using (var sw = new StreamWriter(fileName, true, Encoding.UTF8))
-            {
-                sw.Write("{0} is winner", strnamegamer);
-                sw.WriteLine();
-                sw.WriteLine();
-                sw.WriteLine();
-                sw.WriteLine();
-
-            }
-
-        }
-    }
+            text += strnamegamer += " is winner";
+            text += "\r\n";
+            text += "------\r\n";
+            text += "------\r\n";
+            text += "\r\n";
             
+        }
+
+        public void WriteInFile()
+        {
+            using (var sw = new StreamWriter(this.fileName , true, Encoding.UTF8))
+            {
+                sw.Write(this.text);
+            
+            }
+        }
+           
+         
+
+    }
 }
+
